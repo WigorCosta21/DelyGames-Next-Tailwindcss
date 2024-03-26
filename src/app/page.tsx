a1c47973ef8069a82bcdf1579fc7497d7d3eb62a
wigor <wigor21@hotmail.com>
2024-03-25 23:09:56 -0300

    feat: banner created on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,56 @@
-const Home = () => {
+import Image from "next/image";
+import Link from "next/link";
+
+import { Container } from "@/components/Container";
+import { IGameProps } from "@/utils/types/game";
+
+import { BsArrowRightSquare } from "react-icons/bs";
+
+const getDalyGame = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game_day`,
+      { next: { revalidate: 320 } }
+    );
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+};
+
+const Home = async () => {
+  const dalyGame: IGameProps = await getDalyGame();
+
   return (
-    <div>
-      <h1>Daly Games</h1>
-    </div>
+    <main className="w-full">
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">
+          Separamos um jogo exclusivo para você
+        </h1>
+
+        <Link href={`/game/${dalyGame.id}`}>
+          <section className="w-full bg-black rounded-lg">
+            <div className="w-full max-h-96 h-96 relative rounded-lg">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGame.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+
+              <Image
+                src={dalyGame.image_url}
+                alt={dalyGame.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 33vw"
+                className=" max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+              />
+            </div>
+          </section>
+        </Link>
+      </Container>
+    </main>
   );
 };
 
